af68dfd1a86f320c0934ca65ef59415af401d7e2
hingkop <qkrtjswlswkd@naver.com>
2023-09-01 21:18:45 +0900

    ver1.0

--- accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Account
-
-#Register your models here.
-class AccountAdmin(admin.ModelAdmin) :
-    list_display = ('id', 'password', 'name', 'birth', 'gender', 'height', 'weight', 'last_login', 'is_superuser')
-
-admin.site.register(Account, AccountAdmin)
